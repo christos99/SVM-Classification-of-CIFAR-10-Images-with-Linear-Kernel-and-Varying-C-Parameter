e18392c7975c3a6da41df0fe629866ffb3c3e2fb
christos99 <mangas.99@windowslive.com>
2023-12-21 21:03:18 +0200

    new_sigmoid

--- sigmoid.py
+++ sigmoid.py
@@ -7,78 +7,73 @@ import time
 import warnings
 from sklearn.exceptions import DataConversionWarning
 
+# Suppress warnings related to data conversion in sklearn
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
-# Load CIFAR-10 dataset
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-classesName = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+def load_and_preprocess_data():
+    """
+    Load the CIFAR-10 dataset, normalize and reshape the data.
+    Returns a subset of the dataset for quicker training.
+    """
+    # Load dataset
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# Normalize and reshape the data
-x_train = x_train.astype(np.float)
-x_test = x_test.astype(np.float)
+    # Normalize and reshape
+    x_train, x_test = x_train.astype(np.float32) / 127.5 - 1, x_test.astype(np.float32) / 127.5 - 1
+    x_train, x_test = x_train.reshape((x_train.shape[0], -1)), x_test.reshape((x_test.shape[0], -1))
+    y_train, y_test = y_train.flatten(), y_test.flatten()
 
-x_train = np.reshape(x_train, (x_train.shape[0], -1))
-y_train = np.reshape(y_train, (y_train.shape[0], -1))
-x_test = np.reshape(x_test, (x_test.shape[0], -1))
-y_test = np.reshape(y_test, (y_test.shape[0], -1))
+    # Use a smaller subset for training and testing for efficiency
+    return x_train[:3000], y_train[:3000], x_test[:2000], y_test[:2000]
 
-x_train = ((x_train / 255) * 2) - 1
-x_test = ((x_test / 255) * 2) - 1
+def plot_images(images, labels, predictions, class_names):
+    """
+    Plot a selection of images with their predicted and true labels.
+    """
+    fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i].reshape(32, 32, 3))
+        title = f"True: {class_names[labels[i]]}\nPredicted: {class_names[predictions[i]]}"
+        ax.set_title(title)
+        ax.axis('off')
+    plt.show()
 
-# Working on a smaller dataset (3000 samples for training and 2000 samples for testing)
-x_train = x_train[:3000, :]
-y_train = y_train[:3000, :]
-x_test = x_test[:2000, :]
-y_test = y_test[:2000, :]
-y_test = np.array(np.reshape(y_test, 2000))
-
-
-# Function for plotting images
-def plt_img(x, ax):
-    n_row = 32
-    n_col = 32
-    n_colour = 3
-    x_new = x.reshape((n_row, n_col, n_colour))
-    ax.imshow(x_new)
-
-
-# Support Vector Machine with Sigmoid Kernel
-def sigmoid_svm(c):
-    print(f"Running SVM with sigmoid kernel (c={c}):")
-    clf = SVC(C=c, kernel='sigmoid')
-    start = time.time()
+def train_and_evaluate_svm(x_train, y_train, x_test, y_test, C, kernel='sigmoid'):
+    """
+    Train an SVM model with specified kernel and C value.
+    Evaluate and print its performance on the training and test sets.
+    """
+    print(f"Training SVM with {kernel} kernel (C={C})")
+    start_time = time.time()
+    clf = SVC(C=C, kernel=kernel)
     clf.fit(x_train, y_train)
-    stop = time.time()
-    training_time = stop - start
-    y_pred_train = clf.predict(x_train)
-    y_pred_test = clf.predict(x_test)
-    test_accuracy = accuracy_score(y_test, y_pred_test)
-    train_accuracy = accuracy_score(y_train, y_pred_train)
-    print(f"For c={c}, accuracy on testing set is: {test_accuracy:.4f}, accuracy on training set is: {train_accuracy:.4f}, and training time is equal to: {training_time:.2f} seconds")
+    training_time = time.time() - start_time
 
-    # Plot 4 false predicted images and 4 correctly predicted images for c=0.1
-    if c == 0.1:
-        print("Some image predictions for c = 0.1:")
-        n = 4
-        err = np.where(y_pred_test != y_test)[0]
-        right = np.where(y_pred_test == y_test)[0]
-        f, axarr = plt.subplots(2, n, figsize=(20, 8))
-        for i in range(n):
-            e_i = err[i]
-            r_i = right[i]
-            plt_img(x_test[e_i, :], axarr[0, i])
-            title1 = f"true={classesName[y_test[e_i].astype(int)]} est={classesName[y_pred_test[e_i].astype(int)]}"
-            axarr[0, i].set_title(title1)
-            plt_img(x_test[r_i, :], axarr[1, i])
-            title2 = f"true={classesName[y_test[r_i].astype(int)]} est={classesName[y_pred_test[r_i].astype(int)]}"
-            axarr[1, i].set_title(title2)
+    # Calculate accuracies
+    train_accuracy = accuracy_score(y_train, clf.predict(x_train))
+    test_accuracy = accuracy_score(y_test, clf.predict(x_test))
 
+    print(f"C={C}: Test Acc={test_accuracy:.4f}, Train Acc={train_accuracy:.4f}, Training Time={training_time:.2f}s")
 
-# Running SVM for different c values with sigmoid kernel
-c_svm_sigmoid = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+    return clf
 
-# Iterate over different c values
-for c in c_svm_sigmoid:
-    # Train and evaluate SVM model with sigmoid kernel
-    sigmoid_svm(c)
+def main():
+    class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+    x_train, y_train, x_test, y_test = load_and_preprocess_data()
 
+    # SVM hyperparameters
+    C_values = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+
+    for C in C_values:
+        clf = train_and_evaluate_svm(x_train, y_train, x_test, y_test, C)
+
+        # Plot sample predictions for C=0.1
+        if C == 0.1:
+            predictions = clf.predict(x_test)
+            errors = np.where(predictions != y_test)[0]
+            corrects = np.where(predictions == y_test)[0]
+            sample_indices = np.concatenate([errors[:4], corrects[:4]])
+            plot_images(x_test[sample_indices], y_test[sample_indices], predictions[sample_indices], class_names)
+
+if __name__ == "__main__":
+    main()
