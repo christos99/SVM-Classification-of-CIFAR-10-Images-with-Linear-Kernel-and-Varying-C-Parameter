9248b660e4d6f40a34ca029964d303dc091857c5
christos99 <mangas.99@windowslive.com>
2023-12-21 21:11:39 +0200

    new_combined

--- combined_analysis/combined_svm_classifier.py
+++ combined_analysis/combined_svm_classifier.py
@@ -7,79 +7,93 @@ import time
 import warnings
 from sklearn.exceptions import DataConversionWarning
 
-# Suppress warnings
+# Suppress warnings that occur during data conversion in sklearn
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
-# Function to load and preprocess the CIFAR-10 dataset
 def load_and_preprocess_data():
-    '''
-    Loads the CIFAR-10 dataset and preprocesses it by normalizing and flattening the images.
-    Returns the preprocessed training and testing data.
-    '''
+    """
+    Load and preprocess the CIFAR-10 dataset.
+    Normalize and reshape the data for SVM classification.
+    Returns a subset of the data for training and testing to increase efficiency.
+    """
+    # Load CIFAR-10 dataset
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    # Normalize the data
-    x_train = x_train.astype('float32') / 255.0
-    x_test = x_test.astype('float32') / 255.0
-    # Flatten the images
-    x_train = x_train.reshape((len(x_train), -1))
-    x_test = x_test.reshape((len(x_test), -1))
-    return x_train, y_train, x_test, y_test
 
-# Function to train and evaluate SVM classifier for different C values
-def train_svm_classifier(x_train, y_train, x_test, y_test, kernel_type, C_values):
-    '''
-    Trains and evaluates an SVM classifier for a given kernel type and multiple C values.
-    Returns a dictionary containing training and testing accuracies for each C value.
-    '''
-    results = {}
-    for C in C_values:
-        # Instantiate and train the SVM model
-        svm = SVC(kernel=kernel_type, C=C)
-        svm.fit(x_train, y_train.ravel())
+    # Normalize the data to be within the range [-1, 1]
+    x_train, x_test = x_train.astype(np.float32) / 127.5 - 1, x_test.astype(np.float32) / 127.5 - 1
 
-        # Evaluate the classifier
-        train_accuracy = accuracy_score(y_train, svm.predict(x_train))
-        test_accuracy = accuracy_score(y_test, svm.predict(x_test))
+    # Flatten the images into vectors for SVM classification
+    x_train, x_test = x_train.reshape((x_train.shape[0], -1)), x_test.reshape((x_test.shape[0], -1))
 
-        results[C] = (train_accuracy, test_accuracy)
-    return results
+    # Flatten the labels
+    y_train, y_test = y_train.flatten(), y_test.flatten()
 
-# Function to plot accuracy comparison for different C values
-def plot_accuracy_comparison(results, kernel_type):
-    '''
-    Plots the training and testing accuracies for different C values.
-    '''
-    C_values = list(results.keys())
-    train_accuracies = [results[C][0] for C in C_values]
-    test_accuracies = [results[C][1] for C in C_values]
+    # Return a smaller subset of the data for quick training and testing
+    return x_train[:3000], y_train[:3000], x_test[:2000], y_test[:2000]
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(C_values, train_accuracies, label='Training Accuracy')
-    plt.plot(C_values, test_accuracies, label='Testing Accuracy')
-    plt.xlabel('C value')
-    plt.ylabel('Accuracy')
-    plt.xscale('log')
-    plt.title(f'Accuracy Comparison for {kernel_type} Kernel')
-    plt.legend()
+def plot_images(images, labels, predictions, class_names):
+    """
+    Plot a selection of images showing their true labels and predicted labels.
+    """
+    fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+    for i, ax in enumerate(axes.flat):
+        # Reshape and display the image
+        ax.imshow(images[i].reshape(32, 32, 3))
+
+        # Set title with true and predicted labels
+        title = f"True: {class_names[labels[i]]}\nPredicted: {class_names[predictions[i]]}"
+        ax.set_title(title)
+        ax.axis('off')
     plt.show()
 
-# Main function to execute the script
+def train_and_evaluate_svm(x_train, y_train, x_test, y_test, C, kernel_type):
+    """
+    Train and evaluate an SVM classifier with a specified kernel type and C value.
+    Print the accuracy on the training and testing sets, and the training time.
+    Returns the trained classifier.
+    """
+    print(f"Training SVM with {kernel_type} kernel (C={C})")
+    start_time = time.time()
+
+    # Initialize and train the SVM classifier
+    clf = SVC(C=C, kernel=kernel_type)
+    clf.fit(x_train, y_train)
+
+    # Calculate the time taken to train the classifier
+    training_time = time.time() - start_time
+
+    # Evaluate the classifier
+    train_accuracy = accuracy_score(y_train, clf.predict(x_train))
+    test_accuracy = accuracy_score(y_test, clf.predict(x_test))
+
+    # Print out the results
+    print(f"C={C}: Test Acc={test_accuracy:.4f}, Train Acc={train_accuracy:.4f}, Training Time={training_time:.2f}s")
+    return clf
+
 def main():
-    '''
-    Main function to execute the SVM training and comparison for different kernels and C values.
-    '''
-    # Load and preprocess the CIFAR-10 dataset
+    """
+    Main function to execute the SVM training and evaluation process.
+    Iterates over different kernels and C values for comparison.
+    """
+    class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
     x_train, y_train, x_test, y_test = load_and_preprocess_data()
-    
-    # Define kernels and C values for comparison
+
+    # List of kernels and C values to iterate over
     kernels = ['linear', 'poly', 'sigmoid']
-    C_values = [0.1, 1, 10, 100]
+    C_values = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
 
-    # Train and evaluate SVM classifiers for each kernel and C value
+    # Iterate over each kernel and C value, training and evaluating the SVM classifier
     for kernel in kernels:
-        print(f"Training and evaluating SVM classifiers with {kernel} kernel...")
-        results = train_svm_classifier(x_train, y_train, x_test, y_test, kernel, C_values)
-        plot_accuracy_comparison(results, kernel)
+        for C in C_values:
+            clf = train_and_evaluate_svm(x_train, y_train, x_test, y_test, C, kernel)
 
-if __name__ == '__main__':
+            # For C = 0.1, plot image predictions to visually assess performance
+            if C == 0.1:
+                predictions = clf.predict(x_test)
+                errors = np.where(predictions != y_test)[0]
+                corrects = np.where(predictions == y_test)[0]
+                sample_indices = np.concatenate([errors[:4], corrects[:4]])
+                plot_images(x_test[sample_indices], y_test[sample_indices], predictions[sample_indices], class_names)
+
+if __name__ == "__main__":
     main()
